4e3b2df75df67ffc931a871268f3ceb496d0bb98
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:19:14 -0700

    Create 2859-AddTwoPromises

---
+++ 2859-AddTwoPromises/2859-AddTwoPromises.js
@@ -0,0 +1,16 @@
+// Last updated: 4/29/2026, 10:19:14 AM
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const [res1, res2] = await Promise.all([promise1, promise2]);
+
+    return res1 + res2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
