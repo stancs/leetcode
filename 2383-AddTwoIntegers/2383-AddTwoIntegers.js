c3fa571402731cf9b20b3a351d385603f679734c
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:20:51 -0700

    Create 2383-AddTwoIntegers

---
+++ 2383-AddTwoIntegers/2383-AddTwoIntegers.js
@@ -0,0 +1,9 @@
+// Last updated: 4/29/2026, 10:20:51 AM
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function(num1, num2) {
+    return num1 + num2;
+};
