8f0c81dd1767ab94a4613ba8733bfd26d315bbe8
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:25:15 -0700

    Create 1250-LongestCommonSubsequence

---
+++ 1250-LongestCommonSubsequence/1250-LongestCommonSubsequence.py
@@ -0,0 +1,13 @@
+# Last updated: 4/29/2026, 10:25:15 AM
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        @cache
+        def dp(i, j):
+            if i == len(text1) or j == len(text2):
+                return 0
+            if text1[i] == text2[j]:
+                return 1+dp(i+1, j+1)
+            else:
+                return max(dp(i+1, j), dp(i, j+1))
+        
+        return dp(0, 0)
