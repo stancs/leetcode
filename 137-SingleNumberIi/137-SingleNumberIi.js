1851c4adc6332bc9876d6b97ffbd542a0fbb8178
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:34:07 -0700

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.js
@@ -0,0 +1,21 @@
+// Last updated: 4/29/2026, 10:34:07 AM
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    const map = {};
+    for(const num of nums) {
+        if(map[num] === undefined) {
+            map[num] = 1;
+        } else {
+            map[num]++;
+        }
+    }
+    const keys = Object.keys(map);
+    for(const key of keys) {
+        if(map[key] === 1) {
+            return parseInt(key);
+        }
+    }
+};
