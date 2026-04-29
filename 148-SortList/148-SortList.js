ef4388a82d633acb8a1af1f24f0e0e8efd69e2aa
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:33:52 -0700

    Create 148-SortList

---
+++ 148-SortList/148-SortList.js
@@ -0,0 +1,29 @@
+// Last updated: 4/29/2026, 10:33:52 AM
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+    if(!head) {
+        return null;
+    }
+    const arr = [];
+    let curr = head;
+    while(curr) {
+        arr.push(curr);
+        curr = curr.next;
+    }
+    arr.sort((a, b) => a.val - b.val);
+    for(let i = 0; i < arr.length - 1; i++) {
+        arr[i].next = arr[i+1];
+    }
+    arr[arr.length - 1].next = null;
+    return arr[0];
+};
