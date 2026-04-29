aeb20fd8da94bb807809793874c00deea540befc
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:19:12 -0700

    Create 2860-SortBy

---
+++ 2860-SortBy/2860-SortBy.js
@@ -0,0 +1,9 @@
+// Last updated: 4/29/2026, 10:19:12 AM
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a, b) => fn(a) - fn(b));
+};
