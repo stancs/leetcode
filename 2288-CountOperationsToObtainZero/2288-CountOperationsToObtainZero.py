de416b8752519fb1dd2b658360d07f3d98a8492d
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:21:21 -0700

    Create 2288-CountOperationsToObtainZero

---
+++ 2288-CountOperationsToObtainZero/2288-CountOperationsToObtainZero.py
@@ -0,0 +1,11 @@
+# Last updated: 4/29/2026, 10:21:20 AM
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        count = 0
+        while num1 > 0 and num2 > 0:
+            if num1 >= num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            count += 1
+        return count
