c5bbf99031191d728613b240a1e30bf6d48373c3
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:19:29 -0700

    Create 2796-AllowOneFunctionCall

---
+++ 2796-AllowOneFunctionCall/2796-AllowOneFunctionCall.js
@@ -0,0 +1,22 @@
+// Last updated: 4/29/2026, 10:19:29 AM
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let count = 0;
+    return function(...args){
+        if(count === 0) {
+            count++;
+            return fn(...args);
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
