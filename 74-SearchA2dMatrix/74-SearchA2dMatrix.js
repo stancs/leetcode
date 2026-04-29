b6a74d64d7be4150e1f011639b43f6d2482d4fb9
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:35:12 -0700

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.js
@@ -0,0 +1,31 @@
+// Last updated: 4/29/2026, 10:35:12 AM
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    const arr = [];
+    const m = matrix.length;
+    const n = matrix[0].length;
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            arr.push(matrix[i][j]);
+        }
+    }
+    console.log(arr);
+    let left = 0;
+    let right = m * n - 1;
+    while(left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if(arr[mid] === target) {
+            return true;
+        }
+        if(arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
+};
