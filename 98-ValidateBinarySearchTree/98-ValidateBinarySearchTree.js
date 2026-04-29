d9e508124be7bf4c65069b9b08d4c68c7590a72b
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:34:49 -0700

    Create 98-ValidateBinarySearchTree

---
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.js
@@ -0,0 +1,26 @@
+// Last updated: 4/29/2026, 10:34:49 AM
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    const validate = (node, min, max) => {
+        if(!node) {
+            return true;
+        }
+        if(node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        return validate(node.left, min, node.val) && validate(node.right, node.val, max);
+    }
+    return validate(root, -Infinity, Infinity);
+};
