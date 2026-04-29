092165e6a2368344c7f3cec4d189455bbfa57e63
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:20:16 -0700

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.py
@@ -0,0 +1,6 @@
+# Last updated: 4/29/2026, 10:20:16 AM
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        kelvin = celsius + 273.15
+        fahrenheit = celsius * 1.80 + 32.00
+        return [kelvin, fahrenheit]
