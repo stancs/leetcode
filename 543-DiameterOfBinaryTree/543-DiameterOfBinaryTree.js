fd969bc46307ce0506f0792fbc0493ff9fcf827c
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:29:32 -0700

    Create 543-DiameterOfBinaryTree

---
+++ 543-DiameterOfBinaryTree/543-DiameterOfBinaryTree.js
@@ -0,0 +1,29 @@
+// Last updated: 4/29/2026, 10:29:32 AM
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    let diameter = 0;
+    function depth(node) {
+        if(!node) {
+            return 0;
+        }
+        const leftDepth = depth(node.left);
+        const rightDepth = depth(node.right);
+
+        diameter = Math.max(diameter, leftDepth + rightDepth);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+    depth(root);
+    return diameter;
+};
