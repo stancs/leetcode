b874fd49e29b7f6c42bad7e3bcac303650f64f42
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:36:13 -0700

    Create 33-SearchInRotatedSortedArray

---
+++ 33-SearchInRotatedSortedArray/33-SearchInRotatedSortedArray.js
@@ -0,0 +1,32 @@
+// Last updated: 4/29/2026, 10:36:13 AM
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while(left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        console.log(`left: ${left}, mid: ${mid}, right: ${right}`)
+        
+        if(nums[mid] === target) {
+            return mid;
+        }
+        if(nums[left] <= nums[mid]) {
+            if(nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if(nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
