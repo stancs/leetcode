20611547d3774c1e81484ed090588c2cc7eeba33
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:31:25 -0700

    Create 307-RangeSumQueryMutable

---
+++ 307-RangeSumQueryMutable/307-RangeSumQueryMutable.js
@@ -0,0 +1,36 @@
+// Last updated: 4/29/2026, 10:31:25 AM
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.nums = nums;
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} val
+ * @return {void}
+ */
+NumArray.prototype.update = function(i, val) {
+    this.nums[i] = val;
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    let sum = 0;
+    for(let idx = i; idx <= j; idx++) {
+        sum += this.nums[idx];
+    }
+    return sum;
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * obj.update(i,val)
+ * var param_2 = obj.sumRange(i,j)
+ */
