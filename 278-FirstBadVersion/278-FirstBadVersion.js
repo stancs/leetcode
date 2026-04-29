a1c0ef5973a2d69fe2d9bb56ea65269877dce23d
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:31:54 -0700

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/278-FirstBadVersion.js
@@ -0,0 +1,38 @@
+// Last updated: 4/29/2026, 10:31:54 AM
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let left = 1;
+        let right = n;
+        let prev;
+        while(left < right) {
+            const mid = Math.floor((left + right) / 2);
+            console.log(`left: ${left}, right: ${right}, mid: ${mid}`);
+            console.log(`isBadVersion(${mid})=${isBadVersion(mid)}`);
+            if(isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    };
+};
+
