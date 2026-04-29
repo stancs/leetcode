dd914455296e5fef5cde689355136e6e34513934
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:33:32 -0700

    Create 167-TwoSumIiInputArrayIsSorted

---
+++ 167-TwoSumIiInputArrayIsSorted/167-TwoSumIiInputArrayIsSorted.js
@@ -0,0 +1,21 @@
+// Last updated: 4/29/2026, 10:33:32 AM
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+    while(left < right) {
+        const sum = numbers[left] + numbers[right];
+
+        if(sum === target) {
+            return [left + 1, right + 1];
+        } else if(sum > target) {
+            right--;
+        } else if(sum < target) {
+            left++
+        }
+    }
+};
