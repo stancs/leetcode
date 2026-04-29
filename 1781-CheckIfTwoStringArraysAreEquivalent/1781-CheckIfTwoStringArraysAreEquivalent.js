e82cc892f575b7a7ae03a4be490d582f4ac5269c
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:22:48 -0700

    Create 1781-CheckIfTwoStringArraysAreEquivalent

---
+++ 1781-CheckIfTwoStringArraysAreEquivalent/1781-CheckIfTwoStringArraysAreEquivalent.js
@@ -0,0 +1,11 @@
+// Last updated: 4/29/2026, 10:22:48 AM
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    const str1 = word1.join('');
+    const str2 = word2.join('');
+    return str1 === str2;
+};
