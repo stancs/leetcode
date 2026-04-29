612789cf0ce80902196f406417b4cf64e7e17d5b
Stan Lee <stanley.s.lee@gmail.com>
2026-04-29 10:21:22 -0700

    Create 2277-CountEqualAndDivisiblePairsInAnArray

---
+++ 2277-CountEqualAndDivisiblePairsInAnArray/2277-CountEqualAndDivisiblePairsInAnArray.js
@@ -0,0 +1,17 @@
+// Last updated: 4/29/2026, 10:21:22 AM
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countPairs = function(nums, k) {
+    let count = 0;
+    for(let i = 0; i < nums.length - 1; i++) {
+        for(let j = i+1; j < nums.length; j++) {
+            if(nums[i] === nums[j] && ((i * j) % k === 0)) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
