c466744847da70426d1fbd5bb3255dacb133c89c
Stan Lee <stanley.s.lee@gmail.com>
2026-05-05 17:20:28 -0700

    File updated at 5/5/2026, 5:20:27 PM

--- 61-RotateList/61-RotateList.js
+++ 61-RotateList/61-RotateList.js
@@ -1,44 +1,36 @@
-// Last updated: 4/29/2026, 10:35:35 AM
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @param {number} k
- * @return {ListNode}
- */
-var rotateRight = function(head, k) {
-    if(!head || k === 0) {
-        return head;
-    }
-    const arr = [];
-    let curr = head;
-    while(curr) {
-        arr.push(curr);
-        curr = curr.next;
-    }
-    console.log(`arr.length: ${arr.length}`);
-    const len = arr.length;
-    const remainder = k % len;
-    if(remainder === 0) {
-        return head;
-    }
-    const startIdx = len - remainder;
-    const newHead = arr[startIdx];
-    let count = 0;
-    let node;
-    while(count < len) {
-        const idx = (startIdx + count) % len;
-        node = arr[idx];
-        if(node.next === null) {
-            node.next = arr[0];
-        }
-        count++;
-    }
-    node.next = null;
-    return arr[startIdx];
-};
+// Last updated: 5/5/2026, 5:20:27 PM
+1/**
+2 * Definition for singly-linked list.
+3 * function ListNode(val, next) {
+4 *     this.val = (val===undefined ? 0 : val)
+5 *     this.next = (next===undefined ? null : next)
+6 * }
+7 */
+8/**
+9 * @param {ListNode} head
+10 * @param {number} k
+11 * @return {ListNode}
+12 */
+13var rotateRight = function(head, k) {
+14    if(head === null) {
+15        return head;
+16    }
+17    let len = 0;
+18    let curr = head;
+19    const arr = [];
+20    while(curr !== null) {
+21        arr.push(curr);
+22        len++;
+23        curr = curr.next;
+24    }
+25    console.log(`len: ${len}`);
+26    const r = k % len;
+27    if(r === 0) {
+28        return head;
+29    }
+30    console.log(`r: ${r}`);
+31    const arr2 = arr.slice(len - r).concat(arr.slice(0, len - r));
+32    arr2[r-1].next = arr2[r];
+33    arr2[len-1].next = null;
+34    return arr2[0];
+35};
