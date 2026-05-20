952e03b9811c1dc1a962391a3dfb19c71471b503
Stan Lee <stanley.s.lee@gmail.com>
2026-05-20 14:04:25 -0700

    Create 2657-FindthePrefixCommonArrayofTwoArrays

---
+++ 2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.js
@@ -0,0 +1,30 @@
+// Last updated: 5/20/2026, 2:04:25 PM
+1/**
+2 * @param {number[]} A
+3 * @param {number[]} B
+4 * @return {number[]}
+5 */
+6var findThePrefixCommonArray = function(A, B) {
+7    const len = A.length;
+8    const map = {};
+9    const res = [];
+10    for(let i = 0; i < len; i++) {
+11        const a = A[i];
+12        const b = B[i];
+13        if(map[a] === undefined) {
+14            map[a] = 1;
+15        } else {
+16            map[a]++;
+17        }
+18        if(map[b] === undefined) {
+19            map[b] = -1;
+20        } else {
+21            map[b]--;
+22        }
+23        const values = Object.values(map);
+24        console.log(values);
+25        const count = values.filter(val => val === 0).length;
+26        res.push(count);
+27    }
+28    return res;
+29};
